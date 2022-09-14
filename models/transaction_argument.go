171743a3609095dc6e11c658209203d1960f98d7
Kuma <bear24ice@gmail.com>
2022-09-14 14:25:02 +0800

    fix: enum type order and remove ToString()

--- models/transaction_argument.go
+++ models/transaction_argument.go
@@ -1,16 +1,10 @@
 package models
 
 import (
-	"encoding/hex"
-	"math/big"
-	"strconv"
-
 	"github.com/the729/lcs"
 )
 
-type TransactionArgument interface {
-	ToString() string
-}
+type TransactionArgument interface{}
 
 type TxArgU8 struct {
 	U8 uint8
@@ -24,6 +18,10 @@ type TxArgU128 struct {
 	Higher, Lower uint64
 }
 
+type TxArgBool struct {
+	Bool bool
+}
+
 type TxArgAddress struct {
 	Addr AccountAddress
 }
@@ -32,45 +30,12 @@ type TxArgU8Vector struct {
 	Bytes []byte
 }
 
-type TxArgBool struct {
-	Bool bool
-}
-
 var _ = lcs.RegisterEnum(
 	(*TransactionArgument)(nil),
 	TxArgU8{},
 	TxArgU64{},
 	TxArgU128{},
+	TxArgBool{},
 	TxArgAddress{},
 	TxArgU8Vector{},
-	TxArgBool{},
 )
-
-func (t TxArgU8) ToString() string {
-	return strconv.FormatUint(uint64(t.U8), 10)
-}
-
-func (t TxArgU64) ToString() string {
-	return strconv.FormatUint(t.U64, 10)
-}
-
-func (t TxArgU128) ToString() string {
-	higherBytes := new(big.Int).SetUint64(t.Higher).Bytes()
-	lowerBytes := new(big.Int).SetUint64(t.Lower).Bytes()
-	return new(big.Int).SetBytes(append(higherBytes, lowerBytes...)).String()
-}
-
-func (t TxArgAddress) ToString() string {
-	return hex.EncodeToString(t.Addr[:])
-}
-
-func (t TxArgU8Vector) ToString() string {
-	return hex.EncodeToString(t.Bytes)
-}
-
-func (t TxArgBool) ToString() string {
-	if t.Bool {
-		return "true"
-	}
-	return "false"
-}
--- models/type_tag.go
+++ models/type_tag.go
@@ -1,16 +1,10 @@
 package models
 
 import (
-	"encoding/hex"
-	"fmt"
-	"strings"
-
 	"github.com/the729/lcs"
 )
 
-type TypeTag interface {
-	ToString() string
-}
+type TypeTag interface{}
 
 type TypeTagBool struct{}
 type TypeTagU8 struct{}
@@ -39,45 +33,3 @@ var _ = lcs.RegisterEnum(
 	TypeTagVector{},
 	TypeTagStruct{},
 )
-
-func (t TypeTagBool) ToString() string {
-	return "Bool"
-}
-
-func (t TypeTagU8) ToString() string {
-	return "U8"
-}
-
-func (t TypeTagU64) ToString() string {
-	return "U64"
-}
-
-func (t TypeTagU128) ToString() string {
-	return "U128"
-}
-
-func (t TypeTagAddress) ToString() string {
-	return "Address"
-}
-
-func (t TypeTagSinger) ToString() string {
-	return "Singer"
-}
-
-func (t TypeTagVector) ToString() string {
-	return fmt.Sprintf("vector<%s>", t.TypeTag.ToString())
-}
-
-func (t TypeTagStruct) ToString() string {
-	structType := fmt.Sprintf("%s::%s::%s", "0x"+hex.EncodeToString(t.Address[:]), t.Module, t.Name)
-	if len(t.TypeParams) == 0 {
-		return structType
-	}
-
-	var types []string
-	for _, p := range t.TypeParams {
-		types = append(types, p.ToString())
-	}
-
-	return fmt.Sprintf("%s<%s>", structType, strings.Join(types, ","))
-}
